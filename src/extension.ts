a29ab56c7235a72aa4482e3578851ca4328277c2
Albert Yu <albert.yu@gmail.com>
2026-09-13 21:37:16 -0400

    prefer kebab case

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { OoxmlEditorProvider } from './ooxmlEditorProvider';
+import { OoxmlEditorProvider } from './ooxml-editor-provider';
 
 export function activate(context: vscode.ExtensionContext): void {
   context.subscriptions.push(OoxmlEditorProvider.register(context));
